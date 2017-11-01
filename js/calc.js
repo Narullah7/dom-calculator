c82f5873fbeed1c2476df5e6fcc72e2cf0ad9902
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-11-01 22:30:32 +0000

    in js file did the buttons from 1 to 9 and the AC button

--- js/calc.js
+++ js/calc.js
@@ -1,11 +1,25 @@
 // for loop for the individual buttons
 // numbered 1-9
+var buttons = document.querySelectorAll(".buttonNum");
 
+for (var i = 0; i< buttons.length; i++) {
+  var button = buttons[i];
 
+  button.addEventListener('click',function(event) {
+    console.log(this.value + " was clicked!");
+  })
 
+}
 //button Clear
+var buttonClear = document.getElementById('AC');
+buttonClear.addEventListener('click', function(event){
+  console.log(this.value + " button was clicked");
+  return
+})
 
 // operators *,+,/,-
+
+
 
 //button equals
 
