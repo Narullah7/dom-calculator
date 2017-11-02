cf8fae290d3947b630da66097a0390e4f93b2fc2
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-11-02 00:58:26 +0000

    Corrected code for the AC button, now displays the correct value

--- js/calc.js
+++ js/calc.js
@@ -1,30 +1,43 @@
 // for loop for the individual buttons
 // numbered 1-9
-var buttons = document.querySelectorAll(".buttonNum");
-
-for (var i = 0; i< buttons.length; i++) {
-  var button = buttons[i];
-
-  button.addEventListener('click',function(event) {
-    console.log(this.value + " was clicked!");
-  })
-
-}
+// function Numbers(){
+// var buttons = document.querySelectorAll(".buttonNum");
+//
+// for (var i = 0; i< buttons.length; i++) {
+//   var button = buttons[i];
+//
+//   button.addEventListener('click',function(event) {
+//     var a = console.log(event);
+//   })
+//   return a;
+// }
+// }
 //button Clear
 var buttonClear = document.getElementById('AC');
 buttonClear.addEventListener('click', function(event){
-  console.log(this.value + " button was clicked");
-  return
+  document.getElementById('screen').innerHTML = "0"
 })
 
 // operators *,+,/,-
-
-
-
+// function Operation() {
+// var operators = document.querySelectorAll(".operator");
+//
+// for (var j = 0; j< operators.length; j++) {
+//   var button = operators[j];
+//
+//   button.addEventListener('click',function(event){
+//
+//   })
+// }
+// }
 //button equals
+// var answer = document.getElementById('equals');
+// answer.addEventListener('click',function(event){
+//   console.log(finalAnswer);
+// })
 
-
-//button screen
-
-
-// final answer
+// //button screen
+// var finalAnswer = getElementById('screen');
+//
+// if (buttonClear) {
+// }
